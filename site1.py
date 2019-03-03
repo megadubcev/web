ea99ea2cce0d19b345ff1087650b19f09a9c9d0a
Andrew <megadubcev@gmail.com>
2019-03-03 18:20:08 +0300

    alert

--- site1.py
+++ site1.py
@@ -14,6 +14,24 @@ def image():
     return "<h1> Картинка</h1>" + '''<img src="{}" alt="здесь должна была быть картинка, 
     но не нашлась">'''.format(url_for('static', filename='img/Риана.jpg'))
 
+@app.route('/text_in_alert/<alert>')
+def greeting(alert):
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width,
+                    initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                    crossorigin="anonymous">
+                    <title>{}</title>
+                  </head>
+                  <body>
+                    <h1>{}</h1>
+                  </body>
+                </html>'''.format(alert, alert)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
