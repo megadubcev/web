03d87f97757b9e611dfebb0fb2a9bc2da62d0ebd
Andrew <megadubcev@gmail.com>
2019-03-03 17:23:26 +0300

    siteimg3

--- site1.py
+++ site1.py
@@ -2,11 +2,18 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "<h1>Привет, Яндекс! Я - Андрей</h1>"
+
 @app.route('/image_sample')
 def image():
 
     return "<h1> Картинка</h1>" + '''<img src="{}" alt="здесь должна была быть картинка, 
     но не нашлась">'''.format(url_for('static', filename='img/Риана.jpg'))
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
